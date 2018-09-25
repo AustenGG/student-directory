6d9c261c1b00717a901398730e772f9ab7d9ead4
EC2 Default User <ec2-user@ip-172-31-45-12.us-east-2.compute.internal>
2018-09-25 15:46:34 +0000

    added methods

--- directory.rb
+++ directory.rb
@@ -19,4 +19,33 @@ students.each do |student|
 end
 # finally, we print the total
 puts "Overall, we have #{students.count} great students"
+	students = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+]
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+#nothing happens until we call the methods
+print_header
+print(students)
+print_footer(students)
 	

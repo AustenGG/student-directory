d5b9840254955630bf5267ea4beec3201e8892dc
EC2 Default User <ec2-user@ip-172-31-45-12.us-east-2.compute.internal>
2018-09-26 08:42:59 +0000

    step8excersise1

--- directory.rb
+++ directory.rb
@@ -24,7 +24,7 @@ end
 
 def print(students)
   students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    puts "1. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 

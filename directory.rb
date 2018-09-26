4db78d7b636e24f09d8e9c9eb8e47a8b35a7e25a
EC2 Default User <ec2-user@ip-172-31-45-12.us-east-2.compute.internal>
2018-09-26 10:07:14 +0000

    step8excersise5

--- directory.rb
+++ directory.rb
@@ -4,11 +4,12 @@ def input_students
   # create an empty array
   students = []
   # get the first name
-  name = gets.chomp
+  name = gets.chomp 
+  hobby = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: :november, hobby: hobby}
     puts "Now we have #{students.count} students"
     # get another name from the user
     name = gets.chomp
@@ -23,10 +24,8 @@ def print_header
 end
 
 def print(students)
-  max = students.length - 1
-  i = students.map.with_index
-  puts students
-  until [i] == max
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort) (Favourite hobby: #{student[:hobby]})"
   end
 end
 
